9d435e2363753f7d2f052a2c1a6bb6c466ca6101
emmanuel sam <fndm4@gmail.com>
2025-06-24 12:43:43 +0100

    run build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'build', // 👈 this changes the output folder
+  },
 })
