8b5cb588a1cff9c2f48348af055c70d0adaab5f4
emmanuel sam <fndm4@gmail.com>
2025-08-14 10:57:47 +0200

    land and property

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router";
 import { Button } from "antd";
-import dot from '../../assets/dot.png'
+import linkedin from '../../assets/linkedin.png'
+import twitter from '../../assets/twitter.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import { Links } from "react-router";
 
 
 const Footer = () => {
   return (
-    <div className="py-35 bg-[#333333]">
+    <div className="p-5 bg-[#333333] py-10 pb-10">
       {/* <div className="m-auto w-11/12 grid gap-2 mt-20 pb-5
         grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
         lg:grid-cols-[1.3fr_1fr_1fr_1fr_1fr]">
@@ -60,7 +64,22 @@ const Footer = () => {
           </div>
         </div>
       </div> */}
-      
+      <div className="flex justify-between items-center">
+        <h1 className="font-extrabold text-white text-3xl">HOME</h1>
+        <div className="flex items-center gap-4">
+          <p className="text-white font-semibold">Drive with Us to PROPERTIES</p>
+          <Link to='#' className="text-white font-semibold">Careers</Link>
+          <Link to='#' className="text-white font-semibold">Goals</Link>
+          <Link to='#' className="text-white font-semibold">News</Link>
+          <Link to='#' className="text-white font-semibold">Real Estate</Link>
+        </div>
+      </div>
+      <div className="flex gap-5 mt-8">
+        <img src={facebook} alt="" className="w-6"/>
+        <img src={twitter} alt="" className="w-6"/>
+        <img src={linkedin} alt="" className="w-6"/>
+        <img src={instagram} alt="" className="w-6"/>
+      </div>
     </div>
   );
 };
